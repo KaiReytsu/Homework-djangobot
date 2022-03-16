d4c8fea7aa993e54d25d491c0ca082fc9b8ec2e1
kai.reytsu <kai.reytsu@gmail.com>
2022-03-16 22:23:37 +0300

    fixed html. Add test

--- pollday/polls/tests.py
+++ pollday/polls/tests.py
@@ -1,3 +1,12 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.http import HttpRequest
+from .views import index
 
-# Create your tests here.
+class PollTestCase(TestCase):
+
+    def test_index(self):
+        self.assertEqual(
+            index(HttpRequest()).status_code,
+            HTTPStatus.OK
+        )
